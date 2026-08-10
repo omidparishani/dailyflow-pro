0a26540ab881da32a5d41ca7a353708350e66e7b
OMID <omidparishani@yahoo.com>
2026-08-10 10:31:20 +0330

    3
    3

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-let order=[],idx=0,t=0,int=null,times={};
-const $=id=>document.getElementById(id);
-function draw(){order=[...new Set($('names').value.split('\n').map(x=>x.trim()).filter(Boolean))].sort(()=>Math.random()-.5);idx=0;t=0;render()}
-function render(){$('speaker').innerText=order[idx]||'پایان';$('next').innerText='نفر بعدی: '+(order[idx+1]||'-');$('list').innerHTML=order.map((n,i)=>'<li>'+n+' '+(times[n]?fmt(times[n]):'')+'</li>').join('')}
-function start(){clearInterval(int);int=setInterval(()=>{t++;$('timer').innerText=fmt(t)},1000)}
-function save(){if(order[idx])times[order[idx]]=t}
-function pause(){clearInterval(int);save()}
-function skip(){save();t=0;idx++;render();$('timer').innerText='00:00'}
-function finish(){clearInterval(int);save();let total=Object.values(times).reduce((a,b)=>a+b,0);$('report').innerHTML=order.map((n,i)=>{let p=total?Math.round(times[n]/total*100):0;return n+' '+fmt(times[n]||0)+'<div class="bar" style="width:'+p+'%;background:hsl('+i*80+',70%,45%)">'+p+'%</div>'}).join('')}
-function fmt(s){return String(Math.floor(s/60)).padStart(2,'0')+':'+String(s%60).padStart(2,'0')}
+
+let order=[],index=0,seconds=0,interval=null,times={};
+const $=x=>document.getElementById(x);
+
+function fmt(s){
+ return String(Math.floor(s/60)).padStart(2,"0")+":"+String(s%60).padStart(2,"0")
+}
+function names(){
+ return [...new Set($("names").value.split("\n").map(x=>x.trim()).filter(Boolean))]
+}
+function render(){
+ $("current").textContent=order[index]||"پایان";
+ $("next").textContent="نفر بعدی: "+(order[index+1]||"ندارد");
+ $("list").innerHTML=order.map((n,i)=>`<li>${i+1}. ${n} ${times[n]?fmt(times[n]):""}</li>`).join("");
+}
+function draw(){
+ order=names().sort(()=>Math.random()-.5);
+ index=0;seconds=0;times={};render();
+}
+function save(){
+ if(order[index]) times[order[index]]=seconds;
+}
+$("start").onclick=()=>{
+ clearInterval(interval);
+ interval=setInterval(()=>{
+  seconds++;
+  $("timer").textContent=fmt(seconds);
+ },1000)
+}
+$("pause").onclick=()=>{
+ clearInterval(interval);save();render();
+}
+$("skip").onclick=()=>{
+ save();seconds=0;index++;
+ $("timer").textContent="00:00";
+ render();
+}
+$("finish").onclick=()=>{
+ clearInterval(interval);
+ save();
+ let total=Object.values(times).reduce((a,b)=>a+b,0);
+ $("report").innerHTML=order.map((n,i)=>{
+  let p=total?Math.round((times[n]||0)*100/total):0;
+  return `<div><b>${n}</b> - ${fmt(times[n]||0)}
+  <div class="bar" style="width:${p}%;background:hsl(${i*65},70%,45%)">${p}%</div></div>`
+ }).join("");
+}
+$("draw").onclick=draw;
+render();
